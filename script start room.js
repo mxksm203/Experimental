c2af44b06cf7cf640b30f03bc6c0bc54a6116736
mxksm203 <107805006+mxksm203@users.noreply.github.com>
2023-01-28 07:35:52 +0500

    JS update.
    Updated JavaScript.

--- script start room.js
+++ script start room.js
@@ -1,3 +1,9 @@
+function toexitfirst() {
+    let screens = document.querySelector('.screens');
+    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="gotomirror" value="Назад к зеркалу">';
+    alert("У двери отпала ручка, её никак не открыть. Пути назад нет, только вперёд.");
+}
+
 function toexit() {
     let screens = document.querySelector('.screens');
     screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="gotomirror" value="Назад к зеркалу">';
@@ -5,5 +11,5 @@ function toexit() {
 
 function toportal() {
     let screens = document.querySelector('.screens');
-    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room.jpg"  height="700" width="1100"> </div> <img src="images/Mirror portal sprite.gif" id="mirrorportal"> <input type="button" onclick="toexit()" id="goback" value="Назад">';
+    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room.jpg"  height="700" width="1100"> </div> <img src="images/Mirror portal sprite.gif" id="mirrorportal"> <a href="#" id="nextlevel">Войти в зеркало</a> <input type="button" onclick="toexit()" id="goback" value="Назад">';
 }
