5c74e3f339fe4a4e5c787370b1ba1dce9498a09a
mxksm203 <107805006+mxksm203@users.noreply.github.com>
2023-02-11 07:00:05 +0500

    Update script start room.js

--- script start room.js
+++ script start room.js
@@ -1,15 +1,15 @@
 function toexitfirst() {
     let screens = document.querySelector('.screens');
-    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="gotomirror" value="Назад к зеркалу">';
+    screens.innerHTML = '<div id="mirror-hall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="go-to-mirror" value="Назад к зеркалу">';
     alert("У двери отпала ручка, её никак не открыть. Пути назад нет, только вперёд.");
 }
 
 function toexit() {
     let screens = document.querySelector('.screens');
-    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="gotomirror" value="Назад к зеркалу">';
+    screens.innerHTML = '<div id="mirror-hall"> <img src="images/Mirror portal room back.jpg"  height="700" width="1100"> </div> <input type="button" onclick="toportal()" id="go-to-mirror" value="Назад к зеркалу">';
 }
 
 function toportal() {
     let screens = document.querySelector('.screens');
-    screens.innerHTML = '<div id="mirrorhall"> <img src="images/Mirror portal room.jpg"  height="700" width="1100"> </div> <img src="images/Mirror portal sprite.gif" id="mirrorportal"> <a href="#" id="nextlevel">Войти в зеркало</a> <input type="button" onclick="toexit()" id="goback" value="Назад">';
+    screens.innerHTML = '<div id="mirror-hall"> <img src="images/Mirror portal room.jpg"  height="700" width="1100"> </div> <img src="images/Mirror portal sprite.gif" id="mirror-portal"> <a href="index black maze.html" id="next-level">Войти в зеркало</a> <input type="button" onclick="toexit()" id="go-back" value="Назад">';
 }
